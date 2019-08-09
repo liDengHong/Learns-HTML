dceb6945fe180dcb8a9a8f1dc3737b7b012d7960
lidenghong <lidenghong@show-fitness.com>
2019-08-09 17:54:34 +0800

    ID选择器

---
+++ 基础知识/scripts/index.js
@@ -0,0 +1,11 @@
+var image = document.getElementsByClassName('image');
+console.log(image);
+image.onclick = function(){
+    alert('image');
+}
+
+var p = document.getElementsByClassName('ssdsd')[0];
+console.log(p);
+p.onclick = function() {
+    alert(p);
+}
