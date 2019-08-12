620c9e156814f83f1be646ce855b308d06ae38b1
lidenghong <lidenghong@show-fitness.com>
2019-08-12 15:47:41 +0800

    数据类型,函数等

--- 基础知识/scripts/index.js
+++ 基础知识/scripts/index.js
@@ -1,11 +1,52 @@
+/**添加事件 */
 var image = document.getElementsByClassName('image')[0];
 console.log(image);
-image.onclick = function(){
+image.onclick = function () {
     alert('image');
 }
 
 var p = document.getElementsByClassName('ssdsd')[0];
 console.log(p);
-p.onclick = function() {
+p.onclick = function () {
     alert(p);
 }
+
+/**修改html 中某个选择器的文本 */
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World';
+
+/**test Method */
+function test(q) {
+    console.log(typeof q);
+    return q;
+}
+test(123);
+
+/**数据类型*/
+// 字符串
+let string = '丽丽子';
+//数字
+let number = 10;
+//Boolean
+let isSuccess = true;
+//Array, 可拼装不同类型的数据
+let array = [10, '数字', true];
+//Object 
+let object = document.querySelector('p');
+
+/**条件判断语句 */
+let isSelect = 'select';
+if (isSelect == 'select') {
+    alert('你选择了她');
+} else {
+    alert('你没有选择她');
+}
+
+/**函数 */
+function addCount(numA,numB,numC) {
+    let sum = numA +numB + numC;
+    alert('和是' + sum);
+    return sum;
+}
+
+addCount(12, 34, 454);
